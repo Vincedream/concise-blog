f0356b3d3a359e3adc3505ae72ce6a4e03691178
Vincedream <3161141062@qq.com>
2018-03-24 18:53:27 +0800

    feat: 修改后端部分测试

--- Admin/src/App.js
+++ Admin/src/App.js
@@ -1,18 +1,112 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from './config/axios'
 import './App.css';
+import './test.less'
 
 class App extends Component {
+  async addAticle(){
+    let date = new Date()
+    const addArticle = axios.post('/article/new',{
+      title: '1111112跨域知识点讲解66655511111',
+      content: 'http',
+      views: 1,
+      classify: 'http',
+      comments: [],
+      // comments:["5ab5168cdd2630bf220ce351", "5ab5168cdd2630bf220ce352", "5ab5168cdd2630bf220ce353"],
+      // comments:"5ab5168cdd2630bf220ce353",
+      releaseDate: date.toLocaleDateString()
+    })
+    let result = await addArticle
+    console.log(result)
+  }
+  async register(){
+    const registerUser = axios.post('/register',{
+      username: 'admin1',
+      password: 'admin1'
+    })
+    let result = await registerUser
+    console.log(result)
+  }
+  async login(){
+    const loginUser = axios.post('/login',{
+      username: 'admin1',
+      password: 'admin1'
+    })
+    let result = await loginUser
+    console.log(result)
+  }
+  async keywordfind(){
+    const article = axios.get('/article',{
+      params: {
+        keyword: 'react'
+      }
+    })
+    let result = await article
+  }
+  async findByPage() {
+    const article = axios.get('/article/all/', {
+      params: {
+        page: 1,
+        pageSize: 10
+      }
+    })
+    let result = await article
+  }
+  
+  async getArticleById() {
+    const article = axios.get('/article/'+'5ab5a95f58c354d9b75c1605')
+    let result = await article
+  }
+  async updateArticle() {
+    const update = axios.patch('/article/'+'5ab470404f1be299d67fe142',{
+      title: '2跨域知识点讲解77777',
+      content: 'http',
+      views: 1,
+      classify: 'http',
+    })
+    let result = await update
+    
+  }
+  async deleteArticle() {
+    const delete1 = axios.delete('/article/'+'5ab470404f1be299d67fe142')
+    
+  }
+  async addComment() {
+    const date = new Date()
+    const result = axios.post('/comment',{
+      article: 'asfd',
+      email: 'like@vince.studio',
+      content: 'this is content',
+      createDate: date.toLocaleDateString(),
+      articleId: '5ab5a95f58c354d9b75c1605'
+    })
+  }
+  async getComment(){
+    const result = axios.get('/comment')
+  }
+  async getProject(){
+    const result = axios.get('/project')
+  }
+  async addProject(){
+    const result = axios.post('/project',{
+      name:'vincedrea/nodemail'
+    })
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div >
+      <button onClick={this.addProject.bind(this)}>addProject</button>
+      <button onClick={this.getProject.bind(this)}>getProject</button>
+      <button onClick={this.getComment.bind(this)}>getComment</button>
+      <button onClick={this.addComment.bind(this)}>addComment</button>
+      <button onClick={this.deleteArticle.bind(this)}>deleteArticle</button>
+      <button onClick={this.updateArticle.bind(this)}>updateArticle</button>
+      <button onClick={this.getArticleById.bind(this)}>getArticleById</button>
+      <button onClick={this.findByPage.bind(this)}>findByPage</button>
+      <button onClick={this.keywordfind.bind(this)}>keywordfind</button>
+      <button onClick={this.register.bind(this)}>register</button>
+      <button onClick={this.login.bind(this)}>login</button>
+      <button onClick={this.addAticle.bind(this)}>addArticle</button>
       </div>
     );
   }
---
+++ Admin/src/config/axios.js
@@ -0,0 +1,47 @@
+import axios from 'axios'
+
+import { getCookie } from "../config/token"
+// import history from "../config/history"
+
+const instance = axios.create({
+  timeout: 10000,
+  baseURL: 'http://localhost:3066'
+})
+instance.interceptors.request.use(
+  req => {
+    // const token = getCookie('token')
+    // // 公共请求API,请求头不带有Authorization
+    // const publicUrl = ["/signup", "/signin", "/email/validate", "/user/reset", "/temp/all" ]
+    // const url = req.url
+    // // 其他需要Authorization的请求
+    // if (publicUrl.indexOf(url) === -1) {
+    //   req.headers.Authorization = token
+    //   if (!token){
+    //     // history.push('/login') // 当cookie中存储的token过期后自动跳转到登录页
+    //   }
+    // }
+    return req
+  },
+  err => {
+    throw new Error('发起请求出错')
+  }
+)
+
+instance.interceptors.response.use(
+  res => {
+    return res
+  },
+  err => {
+    console.log(err)
+    // 本地环境错误
+    if (err.message === "Network Error") {
+      throw new Error( '网络环境太差，请稍后再试！')
+    } else if (err.message === "timeout of 5000ms exceeded") {
+      throw new Error( '请求超时，请稍后再试！')
+    } else {
+      throw err   // 非本地环境错误
+    }
+  }
+)
+
+export default instance
