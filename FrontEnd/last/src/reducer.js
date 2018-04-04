10381512efdcd4653a5887aa45675d846dd13aac
Vincedream <3161141062@qq.com>
2018-04-05 01:26:50 +0800

    feat: 修改nav样式

--- FrontEnd/last/src/reducer.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-
-import { article } from './redux/article.redux';
-import { photo } from './redux/photo.redux';
-
-export default combineReducers({ article, photo })
