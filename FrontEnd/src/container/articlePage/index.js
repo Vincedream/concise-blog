0f6e6bc9403e3673e9a48816b5d9d500d8a806c5
Vincedream <3161141062@qq.com>
2018-04-06 16:08:00 +0800

    fix：修改window系统下的滚动条bug

--- FrontEnd/src/container/articlePage/index.js
+++ FrontEnd/src/container/articlePage/index.js
@@ -118,7 +118,7 @@ class About extends React.Component {
  
   render() {
     return (
-      <div  style={{overflow: "scroll",width: "780px",position: "absolute",left: "50%",marginTop: "330px",marginLeft: "-390px"}}>
+      <div  style={{width: "780px",position: "absolute",left: "50%",marginTop: "330px",marginLeft: "-390px"}}>
         {this.state.data ? (
        <div>
           <ContentBox data={this.state.data} />
