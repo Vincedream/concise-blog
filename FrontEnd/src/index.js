5691e02722c6961fb0977d94e46e14662f2f0a16
Vincedream <3161141062@qq.com>
2018-04-09 10:18:25 +0800

    feat: 新增样式

--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -22,7 +22,7 @@ const store = createStore(
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter >
       <Router history={history}>
         <App />
       </Router>
